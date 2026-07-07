6c4076856961dc5fa7bff821c2c3d6a943fa8fb2
Alexey <geheim79@mail.ru>
2026-07-07 19:34:49 +0500

    Кнопка с переключением текста

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import "./App.css";
 
-
-import './App.css'
+import { useState } from "react";
 
 function App() {
 
+  const [stateOn, stateOff] = useState("Вкл");
+  function ToggleButton() {
+    
+     stateOn === "Вкл" ?   
+     stateOff ("Выкл") 
+     : stateOff ("Вкл")
+  }
 
   return (
     <>
-    
+      <button onClick={ToggleButton}>{stateOn}</button>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
