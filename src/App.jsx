6d89202ee28e1599dd81252c33bb2e34f408c78c
Geheim79 <geheim79@mail.ru>
2026-07-09 14:08:26 +0500

    Счетчик работает без ограничения

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  if (count > 10 ) {
+    
+    setCount ("Счётчик не может быть больше 10!" )
+  } else if (count < 0 ){
+     setCount ("Счётчик не может быть меньше 0!" )
+  }
 
   return (
     <>
-    
+      <div>
+        <p>Счётчик: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Увеличить</button>
+
+        <button onClick={() => setCount(count - 1)}>Уменьшить</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
