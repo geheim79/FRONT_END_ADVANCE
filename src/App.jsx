a609276c905033a5b035bfe4d5e9a5f5264099c7
Geheim79 <geheim79@mail.ru>
2026-07-10 15:53:08 +0500

    all working fine

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,39 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [message, setMessage] = useState(0);
+  const [message, setMessage] = useState("");
 
- function handleClickUp() {
-  
-  if (count > 10 ){
-    setCount (count = 0)
+  function handleClickUp() {
+    if (count > 9 ) {
+      // setCount(0);
+      // return
+      setMessage("Счётчик не может быть больше 10!");
+    } else {
+      setCount(count + 1);
+      setMessage ("")
+    }
   }
-  console.log(useState)
-  
- }
 
- function handleClickDown () {
-  
- }
+  function handleClickDown() {
+    // 
+    if (count < 1) {
+      setMessage("Счётчик не может быть меньше 0!");
+      setCount(count -0 );
+      // return
+      
+    }else {
+      setCount (count -1)
+      setMessage("")
+      
+      // 
+      // setMessage("Счётчик не может быть меньше 0!");
+  }
+}
 
   return (
     <>
       <div>
+        <p>{message}</p>
         <p>Счётчик: {count}</p>
         <button onClick={handleClickUp}>Увеличить</button>
 
