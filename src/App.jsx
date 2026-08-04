7faa2c77be90c37306244f4e9cc8341d6cbd9bc6
Alexey <geheim79@mail.ru>
2026-08-04 22:42:43 +0500

    lesson-2-homework-task3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+// Цель: Создать компонент, который при размонтировании устанавливает флаг isMounted = false, и использовать его, чтобы избежать обновления состояния в несуществующем компоненте.
 
-
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(false);
+  const [isMounted, setIsMounted] = useState(true);
 
 
+  // TODO: через 2 секунды после монтирования установить isOnline = true
+  // Но если компонент размонтирован — не делать setIsOnline
+
+  useEffect(() => {
+    console.log("Компонент cмонтирован");
+    //  оборачиваем таймером в 2 сек условие
+    const timer = setTimeout(() => {
+      if (isMounted === true) {
+        setIsOnline(true);
+      }
+    }, 2000);
+
+    // размонтирование компонента
+    return () => {
+      console.log("Компонент размонтирован");
+      // флаг состояния размонтированного компонента
+      setIsMounted(false);
+      clearTimeout(timer);
+    };
+  }, []);
+  // console.log("isOnline", isOnline);
+  // console.log("isMounted", isMounted);
+  //  отрисовка статуса - если isOnline статус true то вывести "Онлайн" и наоборот
   return (
     <>
-    
+      <p>Статус: {isOnline ? "Онлайн" : "Оффлайн"}</p>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import RenderCounter from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
     <App />
+    
 
 )
